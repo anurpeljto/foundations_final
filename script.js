8fb188f2ce56c3a79ea6ae31f3a8b7e19df2c2ae
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2023-11-29 16:21:16 +0100

    FIX equals button.
    I fixed the equals button functionality

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const number1 = 0;
 const operation = 0;
 const number2 = 0;
 
-function addTwo(num1, num2){ return num1+num2};
+function addTwo(num1, num2){ return num1 + num2};
 function subTwo(num1, num2) { return num1 - num2};
 function divTwo(num1, num2) { if (num2 != 0) { return num1/num2} else { return "Error"}};
 function mulTwo(num1, num2) {return num1 * num2};
@@ -21,6 +21,14 @@ function operate(number, number2, operation){
  }
  const result = document.querySelector(".display_results");
  const dig1 = document.querySelector("#one");
+ const dig2 = document.querySelector("#two");
+ const dig3 = document.querySelector("#three");
+ const dig4 = document.querySelector("#four");
+ const dig5 = document.querySelector("#five");
+ const dig6 = document.querySelector("#six");
+ const dig7 = document.querySelector("#seven");
+ const dig8 = document.querySelector("#eight");
+ const dig9 = document.querySelector("#nine");
  const equals = document.querySelector(".equals");
  const add = document.querySelector(".add");
  const sub = document.querySelector(".sub");
@@ -36,68 +44,142 @@ function operate(number, number2, operation){
     }
  });
 
+ dig2.addEventListener("click", () => {
+    if(result.textContent == "Result will be listed here" || result.textContent == ""){
+        result.textContent = "2";
+    }
+    else {
+        result.textContent += "2";
+    }
+ });
+
+ dig3.addEventListener("click", () => {
+    if(result.textContent == "Result will be listed here" || result.textContent == ""){
+        result.textContent = "3";
+    }
+    else {
+        result.textContent += "3";
+    }
+ });
+
+ dig4.addEventListener("click", () => {
+    if(result.textContent == "Result will be listed here" || result.textContent == ""){
+        result.textContent = "4";
+    }
+    else {
+        result.textContent += "4";
+    }
+ });
+
+ dig5.addEventListener("click", () => {
+    if(result.textContent == "Result will be listed here" || result.textContent == ""){
+        result.textContent = "5";
+    }
+    else {
+        result.textContent += "5";
+    }
+ });
+
+ dig6.addEventListener("click", () => {
+    if(result.textContent == "Result will be listed here" || result.textContent == ""){
+        result.textContent = "6";
+    }
+    else {
+        result.textContent += "6";
+    }
+ });
+
+ dig7.addEventListener("click", () => {
+    if(result.textContent == "Result will be listed here" || result.textContent == ""){
+        result.textContent = "7";
+    }
+    else {
+        result.textContent += "7";
+    }
+ });
+
+ dig8.addEventListener("click", () => {
+    if(result.textContent == "Result will be listed here" || result.textContent == ""){
+        result.textContent = "8";
+    }
+    else {
+        result.textContent += "8";
+    }
+ });
+
+ dig9.addEventListener("click", () => {
+    if(result.textContent == "Result will be listed here" || result.textContent == ""){
+        result.textContent = "9";
+    }
+    else {
+        result.textContent += "9";
+    }
+ });
 
 
  add.addEventListener("click", () => {
     result.textContent += "+";
  });
 
- sub.addEventListener("click", () => result.textContent += "-");
- mult.addEventListener("click", () => result.textContent += "*");
- div.addEventListener("click", () => result.textContent += "/");
+sub.addEventListener("click", () => result.textContent += "-");
+mult.addEventListener("click", () => result.textContent += "*");
+div.addEventListener("click", () => result.textContent += "/");
 
-
-
-
- equals.addEventListener("click", () => {
-    let numbers = result.textContent.split('');
-    let numbersToInt = numbers.map(Number);
-    console.log(numbersToInt); // map numbers array into another array by digit so that we could later split that array into two parts separated by operator
-    let firstNumber = [];
-    let secondNumber = [];
-
-    let i = 0;
-    let counter = 0;
-    while(Number.isInteger(numbersToInt[i])){
-        firstNumber.push(numbersToInt[i]);
-        i++;
-        counter++;
-    }
-    firstNumber = Number(firstNumber.join(""));
-
-    let secondCounter = 0;
-    while(!Number.isInteger(numbersToInt[counter+secondCounter])){
-        secondCounter++;
-    }
-
-    let endOperator = counter + secondCounter;
-
-    let operator = numbers[counter];     // getting the operator from numbers array using counter
-    counter++; // so that the counter moves from the "NaN" element on to the next number
-
-    while(counter < numbersToInt.length){
-        secondNumber.push(numbersToInt[counter]);
-        counter++;
-    }
-    secondNumber = Number(secondNumber.join(""));
-    
-    switch(operator){
-        case("+"):
-        result.textContent = addTwo(firstNumber, secondNumber);
-        case("-"):
-        result.textContent = subTwo(firstNumber, secondNumber);
-        case("/"):
-        result.textContent = divTwo(firstNumber, secondNumber);
-        case("*"):
-        result.textContent = mulTwo(firstNumber, secondNumber);
-    }
- });
-
- const clear = document.querySelector(".clear");
+const clear = document.querySelector(".clear");
 clear.addEventListener("click", () => {
     result.textContent = "";
 
 });
+
+// document equals
+
+equals.addEventListener("click", () => {
+    let contentString = result.textContent;
+    let contentArray = contentString.split(''); // get each number/element individually so that we could later identify operator
+
+    let part1 = [];
+    let part2 = [];
+    let operator = "";
+    let index = -1;
+    contentArray.forEach((item) => {
+        if (index != -1){
+            // skip
+        }
+        else {
+        if (item == "+" || item == "-" || item == "/" || item == "*"){
+            index = contentArray.indexOf(item);
+            operator = item;
+        }
+    }
+    });
+
+    for (let i = 0; i < index; i ++){
+        part1.push(contentArray[i]);
+    }
+    for (let j = index+1; j < contentArray.length; j++){
+        part2.push(contentArray[j]);
+    }
+
+    // now i must convert part1 and part2 to int so i could do operations with them 
+
+    part1 = part1.join('');
+    part2 = part2.join('');
+
+    let final = 0;
+    switch(operator){
+        case "+":
+            final = addTwo(+part1, +part2);
+        case "-":
+            final =  subTwo(+part1, +part2);
+        case "*":
+            final = mulTwo(+part1, +part2);
+        case "/":
+            final = divTwo(+part1, +part2);
+    }
+    result.textContent = final;
+
+})
+
 
 
 
